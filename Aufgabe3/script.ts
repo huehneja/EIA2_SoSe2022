2b3f220f0fb77d383d4a89785069379bb8a8c779
huehneja <jason.huehne@hs-furtwangen.de>
2022-04-25 10:38:54 +0200

    Update script.ts

--- Aufgabe3/script.ts
+++ Aufgabe3/script.ts
@@ -68,7 +68,7 @@ namespace MemorySettings {
             wrapper.replaceChildren();
             memorySet = ["25", "24", "23", "22", "21", "20", "19", "18", "17", "16", "15", "14", "13", "12", "11", "10", "09", "08", "07", "06", "05", "04", "03", "02", "01"]; 
             form.style.display = "inline";
-        }
+        } //Ich habe hier testweise noch hinzugefügt, dass das Spiel wieder von vorne startet, wenn es gewonnen wurde, steht nicht in meinem AD drin
         
     }
     interface FirstCard {value: string; position: HTMLElement; }
