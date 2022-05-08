6376ae58b4f98a72a02cb93a8e5f1caff5e8b51e
huehneja <jason.huehne@hs-furtwangen.de>
2022-05-08 12:48:09 +0200

    Art

--- Aufgabe8/script.ts
+++ Aufgabe8/script.ts
@@ -12,49 +12,60 @@ function hndLoad(_event: Event): void {
     let canvas: HTMLCanvasElement = <HTMLCanvasElement>document.querySelector("canvas");
     let crc2: CanvasRenderingContext2D = <CanvasRenderingContext2D> canvas.getContext("2d");
     drawBackground();
-    for (let i: number = 0; i <= Math.floor(Math.random() * 200); i++) {
+    for (let i: number = 0; i <= Math.floor(Math.random() * 50); i++) {
     crc2.lineWidth = Math.floor(Math.random() * 5);
     drawCircle();
     drawRect();
     drawCurve();
     drawLine();
     }
-    setTimeout(function(): void {location.reload(); }, 2000); 
-    function drawBackground(): void {
-        let gradient: CanvasGradient = crc2.createLinearGradient(0, 0, crc2.canvas.width, crc2.canvas.height);
+}
 
-        gradient.addColorStop(0, `rgb(${Math.random() * 256}, ${Math.random() * 256}, ${Math.random() * 256})`);
-        gradient.addColorStop(Math.random(), `rgb(${Math.random() * 256}, ${Math.random() * 256}, ${Math.random() * 256})`);
-        gradient.addColorStop(1, `rgb(${Math.random() * 256}, ${Math.random() * 256}, ${Math.random() * 256})`);
+function drawBackground(): void {
+    let canvas: HTMLCanvasElement = <HTMLCanvasElement>document.querySelector("canvas");
+    let crc2: CanvasRenderingContext2D = <CanvasRenderingContext2D> canvas.getContext("2d");
+    let gradient: CanvasGradient = crc2.createLinearGradient(0, 0, crc2.canvas.width, crc2.canvas.height);
 
-        crc2.fillStyle = gradient;
-        crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+    gradient.addColorStop(0, `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`);
+    gradient.addColorStop(Math.random(), `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`);
+    gradient.addColorStop(1, `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`);
+
+    crc2.fillStyle = gradient;
+    crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
     }
-    function drawCircle(): void {
-        crc2.strokeStyle = `rgb(${Math.random() * 256}, ${Math.random() * 256}, ${Math.random() * 256})`;
-        crc2.beginPath();
-        crc2.ellipse(Math.random() * canvas.width, Math.random() * canvas.height, Math.random() * 100, Math.random() *  75, Math.random() *  Math.PI, 0, 2 * Math.PI);
-        crc2.stroke();
+function drawCircle(): void {
+    let canvas: HTMLCanvasElement = <HTMLCanvasElement>document.querySelector("canvas");
+    let crc2: CanvasRenderingContext2D = <CanvasRenderingContext2D> canvas.getContext("2d");
+    crc2.strokeStyle = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+    crc2.beginPath();
+    crc2.ellipse(Math.random() * canvas.width, Math.random() * canvas.height, Math.random() * canvas.width, Math.random() * canvas.width, Math.random() *  Math.PI,  0, 2 * Math.PI);
+    crc2.stroke();
     }
-    function drawRect(): void {
-        crc2.strokeStyle = `rgb(${Math.random() * 256}, ${Math.random() * 256}, ${Math.random() * 256})`;
-        crc2.beginPath();
-        crc2.rect(Math.random() * canvas.width, Math.random() * canvas.height, Math.random() * 100, Math.random() *  75);
-        crc2.stroke();
-    }
-    function drawCurve(): void {
-        crc2.strokeStyle = `rgb(${Math.random() * 256}, ${Math.random() * 256}, ${Math.random() * 256})`;
-        crc2.moveTo(Math.random() * canvas.width, 0);
-        crc2.quadraticCurveTo(Math.random() * canvas.width, canvas.height, Math.random() * canvas.width, canvas.height);
-        crc2.stroke();
+function drawRect(): void {
+    let canvas: HTMLCanvasElement = <HTMLCanvasElement>document.querySelector("canvas");
+    let crc2: CanvasRenderingContext2D = <CanvasRenderingContext2D> canvas.getContext("2d");
+    crc2.strokeStyle = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+    crc2.beginPath();
+    crc2.rect(Math.random() * canvas.width, Math.random() * canvas.height, Math.random() * canvas.height, Math.random() * canvas.width);
+    crc2.stroke();
+}
+function drawCurve(): void {
+    let canvas: HTMLCanvasElement = <HTMLCanvasElement>document.querySelector("canvas");
+    let crc2: CanvasRenderingContext2D = <CanvasRenderingContext2D> canvas.getContext("2d");
+    crc2.strokeStyle = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+    crc2.moveTo(Math.random() * canvas.width, Math.random() *  canvas.height);
+    crc2.quadraticCurveTo(Math.random() * canvas.width, canvas.height, Math.random() * canvas.width, Math.random() *  canvas.height);
+    crc2.stroke();
 
     }
-    function drawLine(): void {
-        crc2.strokeStyle = `rgb(${Math.random() * 256}, ${Math.random() * 256}, ${Math.random() * 256})`;
-        crc2.moveTo(Math.random() * canvas.width, 0);
-        crc2.lineTo(Math.random() * canvas.width, canvas.height);
-        crc2.stroke();
+function drawLine(): void {
+    let canvas: HTMLCanvasElement = <HTMLCanvasElement>document.querySelector("canvas");
+    let crc2: CanvasRenderingContext2D = <CanvasRenderingContext2D> canvas.getContext("2d");
+    crc2.strokeStyle = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+    crc2.moveTo(Math.random() * canvas.width, Math.random() *  canvas.height);
+    crc2.lineTo(Math.random() * canvas.width, Math.random() *  canvas.height);
+    crc2.stroke();
 
     }
 
-}}
+}
